be8fbcdef70ee646d7d33630f9f02f7cf4faaa8e
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-09-08 08:09:04 +0000

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\génielogiciel\python\learning\mlpython\mldjango\clever programmer\mysite\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
