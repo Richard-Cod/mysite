afb11f31e9f474c6a1d0a8fe5e5b14566536dfaa
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-09-08 16:13:50 +0000

    Add the feature to edit blog post from frontend

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def post_detail(request,id):
 	post= get_object_or_404(Post,pk=id)
 	return render(request,'blog/posts_detail.html',{'post':post})
 
-def post_edit(request):
+def post_new(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
 		if form.is_valid():
@@ -22,8 +22,21 @@ def post_edit(request):
 			post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
+			return redirect('post_detail',id=post.id)
+	else:
+		form = PostForm()
+	return render(request,'blog/post_edit.html',{'form':form})
+
+def post_edit(request,id):
+	post = get_object_or_404(Post,id=id)
+	if request.method == 'POST':
+		form = PostForm(request.POST,instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
 			return redirect('post_detail',id=post.pk)
 	else:
-		print("Un biz tout vide")
-		form = PostForm()
+		form = PostForm(instance=post)
 	return render(request,'blog/post_edit.html',{'form':form})
