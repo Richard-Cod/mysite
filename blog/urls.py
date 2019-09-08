38d37d98a1852bb28231c7794844bbfac4afa44b
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-09-08 10:31:15 +0000

    add the ability to view the detail for each post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.post_list_view,name='post_list'),
+    path('post/<int:id>/',views.post_detail,name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -8,3 +8,7 @@ from .models import Post
 def post_list_view(request):
 	posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request,'blog/posts_list.html',{'posts':posts})
+
+def post_detail(request,id):
+	post= get_object_or_404(Post,pk=id)
+	return render(request,'blog/posts_detail.html',{'post':post})
