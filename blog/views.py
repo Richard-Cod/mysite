43489516a11bd687be634850fce679b085eb3f60
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-09-08 12:25:41 +0000

    add feature to create new blog post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 ,redirect
 from django.utils import timezone
 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,3 +13,17 @@ def post_list_view(request):
 def post_detail(request,id):
 	post= get_object_or_404(Post,pk=id)
 	return render(request,'blog/posts_detail.html',{'post':post})
+
+def post_edit(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail',id=post.pk)
+	else:
+		print("Un biz tout vide")
+		form = PostForm()
+	return render(request,'blog/post_edit.html',{'form':form})
