e1ab2294c0dbe162376aea58aab856a88c8a0d2a
Bathiebo Dieudonne joseph <richard-brice-moussa.bathiebo@epitech.eu>
2021-01-12 10:50:38 +0100

    Fix design and add Requirements.txt

--- blog/urls.py
+++ blog/urls.py
@@ -6,12 +6,17 @@ from django.urls import path
 urlpatterns = [
     path('',views.post_list_view,name='post_list'),
     path('post/<int:id>/',views.post_detail,name='post_detail'),
+
     path('post/new/',views.post_new,name='post_new'),
     path('post/edit/<int:id>',views.post_edit,name='post_edit'),
+
     path('post/draft/',views.draft_list_view,name='post_draft'),
+
     path('post/draft/publish/<int:id>',views.draft_list_publish,name='post_publish'),
+
     path('post/comment/<int:id>',views.comment_to_post,name='comment_to_post'),
     path('post/comment/remove/<int:id>',views.remove_comment,name='remove_comment'),
+    
     path('post/comment/approve/<int:id>',views.approve_comment,name='approve_comment'),
 
     path('signup',views.signup,name='signup'),
